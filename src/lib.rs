c83994bbfaad0835cc1d95180816041ce720e486
Curtis Maves <curtismaves@gmail.com>
2020-08-10 22:43:34 -0500

    Larger reorg

---
+++ src/lib.rs
@@ -0,0 +1,179 @@
+
+use rustable::{Error as BLEError, ValOrFn, MAX_APP_MTU};
+use rustable::gatt::{LocalCharactersitic, CharValue};
+use sha2::{Digest, Sha256};
+
+pub struct OutSyncer {
+    buf: Vec<u8>,
+    cur_pos: usize,
+    written: usize,
+    hash: [u8; 32],
+    verbose: u8,
+}
+
+impl OutSyncer {
+    pub fn new(buf: Vec<u8>, verbose: u8) -> Self {
+        let hash: [u8; 32] = Sha256::digest(&buf).into();
+        OutSyncer {
+            hash,
+            buf,
+            cur_pos: std::usize::MAX,
+            written: 0,
+            verbose,
+        }
+    }
+	pub fn get_buf(&self) -> &[u8] {
+		&self.buf
+	}
+    pub fn indicate_local(
+        &mut self,
+        local_char: &mut LocalCharactersitic,
+        max_out: usize,
+    ) -> Result<(), BLEError> {
+        if self.buf.len() == self.cur_pos {
+            return Ok(());
+        }
+        let mut vf = ValOrFn::Value(CharValue::default());
+        local_char.write_val_or_fn(&mut vf); // remove the vf function so we can write custom data
+        if self.cur_pos == std::usize::MAX {
+            // we need to write the initial data to tell the GATT client how long what we are copying is
+            let mut v = CharValue::default();
+            v.extend_from_slice(&std::u32::MAX.to_be_bytes());
+            v.extend_from_slice(&(self.buf.len() as u32).to_be_bytes());
+            v.extend_from_slice(&self.hash);
+            local_char.write_val_or_fn(&mut ValOrFn::Value(v));
+            if let Err(e) = local_char.notify() {
+                local_char.write_val_or_fn(&mut vf);
+                return Err(e);
+            }
+            self.written = 0;
+            local_char.write_val_or_fn(&mut vf); // return read function
+            return Ok(());
+        }
+        let target = self.buf.len().min(self.cur_pos + max_out);
+        while self.written < target {
+            let end = target.min(self.written + MAX_APP_MTU - 8);
+            let mut v = CharValue::default();
+            let l = end - self.written;
+            v.extend_from_slice(&self.written.to_be_bytes()[4..8]);
+            v.extend_from_slice(&l.to_be_bytes()[4..8]);
+            v.extend_from_slice(&self.buf[self.written..end]);
+            local_char.write_val_or_fn(&mut ValOrFn::Value(v));
+            if self.verbose > 0 {
+                eprintln!("Indicating at position {}.", self.written);
+            }
+            if let Err(e) = local_char.notify() {
+                local_char.write_val_or_fn(&mut vf);
+                return Err(e);
+            }
+            self.written = end;
+        }
+        local_char.write_val_or_fn(&mut vf);
+        Ok(())
+    }
+    pub fn read_fn(&self) -> CharValue {
+        if self.cur_pos == std::usize::MAX {
+            let mut v = CharValue::default();
+            v.extend_from_slice(&std::u32::MAX.to_be_bytes());
+            v.extend_from_slice(&(self.buf.len() as u32).to_be_bytes());
+            v.extend_from_slice(&self.hash);
+            v
+        } else {
+            let end = self.buf.len().min(self.cur_pos + 256 - 8);
+            let mut v = CharValue::default();
+            // let l = end - self.cur_pos;
+            let l = match end.checked_sub(self.cur_pos) {
+                Some(v) => v,
+                None => panic!("overflowed end - self.cur_pos: {} - {}", end, self.cur_pos),
+            };
+            v.extend_from_slice(&(self.cur_pos as u32).to_be_bytes());
+            v.extend_from_slice(&(l as u32).to_be_bytes());
+            v.extend_from_slice(&self.buf[self.cur_pos..end]);
+            v
+        }
+    }
+    /// If this function returns `true` it indicates there is still data to be written to the client.
+    pub fn update_pos(&mut self, data: &[u8]) {
+        let mut int_buf = [0; 4];
+        int_buf.copy_from_slice(&data[..4]);
+        let cur_pos = u32::from_be_bytes(int_buf);
+        int_buf.copy_from_slice(&data[4..8]);
+        let msg_length = u32::from_be_bytes(int_buf) as usize;
+        let mut hash = [0; 32];
+        hash.copy_from_slice(&data[8..40]);
+        if msg_length != self.buf.len() || hash != self.hash || cur_pos as usize > self.buf.len() {
+            // handle hash or msg_length mismatch
+            self.cur_pos = std::usize::MAX;
+        } else {
+            // self.dirty = cur_pos as usize != self.buf.len();
+            self.cur_pos = if cur_pos == std::u32::MAX {
+                // we have no received
+                std::usize::MAX
+            } else {
+                let cur_pos = cur_pos as usize;
+                if cur_pos <= self.cur_pos {
+                    // a duplicate ACK was received
+                    self.written = cur_pos
+                }
+                cur_pos
+            };
+        }
+    }
+}
+
+
+
+#[derive(Default)]
+pub struct InSyncer {
+    hash: [u8; 32],
+    msg_length: usize,
+    data_buf: Vec<u8>,
+}
+impl InSyncer {
+    pub fn read_fn(&self) -> CharValue {
+        let mut ret = CharValue::default();
+        ret.extend_from_slice(&self.data_buf.len().to_be_bytes()[4..8]);
+        ret.extend_from_slice(&self.msg_length.to_be_bytes()[4..8]);
+        ret.extend_from_slice(&self.hash);
+        return ret;
+    }
+    pub fn process_write(&mut self, v: &[u8]) -> Result<Option<Vec<u8>>, ()> {
+        let s: &Self = self;
+        if v.len() < 8 {
+            return Err(());
+        }
+        let mut int_buf = [0; 4];
+        int_buf.copy_from_slice(&v[..4]);
+        let p_num = u32::from_be_bytes(int_buf);
+        int_buf.copy_from_slice(&v[4..8]);
+        let p_len = u32::from_be_bytes(int_buf) as usize;
+        if p_num == std::u32::MAX {
+            if v.len() != 40 {
+                let s: &Self = self;
+                return Err(());
+            }
+            self.data_buf.clear();
+            self.msg_length = p_len;
+            self.hash.copy_from_slice(&v[8..])
+        } else {
+            if p_len + 8 != v.len() {
+                return Err(());
+            }
+            if self.data_buf.len() + p_len > self.msg_length {
+                return Err(());
+            }
+            self.data_buf.extend_from_slice(&v[8..]);
+        }
+        if self.data_buf.len() == self.msg_length {
+            let res = Sha256::digest(&self.data_buf);
+            if res.as_slice() == self.hash {
+                Ok(Some(self.data_buf.clone()))
+            } else {
+                self.data_buf.clear();
+                Err(())
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::process::{Command, Stdio};
 use std::rc::Rc;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+use airboard_server::{InSyncer, OutSyncer};
+
 
 const COPY_UUID: &'static str = "4981333e-2d59-43b2-8dc3-8fedee1472c5";
 const READ_UUID: &'static str = "07178017-1879-451b-9bb5-3ff13bb85b70";
@@ -62,10 +64,10 @@ fn main() {
     let mut read_flags = CharFlags::default();
     // perimissions
     read_flags.secure_read = true;
-    read_flags.read = true;
+    read_flags.encrypt_read = true;
     read_flags.notify = true;
     read_flags.indicate = true;
-    read_flags.write = true;
+    read_flags.encrypt_write = true;
     read_flags.write_wo_response = true;
     // create read characteristic
     let read_uuid = Rc::from(READ_UUID.to_string());
@@ -88,9 +90,9 @@ fn main() {
     let write_uuid = Rc::from(WRITE_UUID.to_string());
     let mut write_flags = CharFlags::default();
     write_flags.secure_write = true;
-    write_flags.write = true;
+    write_flags.encrypt_write = true;
     write_flags.write_wo_response = true;
-    write_flags.read = true;
+    write_flags.encrypt_read = true;
     write_flags.notify = true;
     write_flags.indicate = true;
     let mut write_char = LocalCharBase::new(&write_uuid, write_flags);
@@ -111,8 +113,8 @@ fn main() {
             }
             Err(_) => true,
         };
-        let (v, l) = syncer.read_fn();
-        Ok((Some(ValOrFn::Value(v, l)), true))
+        let cv = syncer.read_fn();
+        Ok((Some(ValOrFn::Value(cv)), true))
     }));
     copy_service.add_char(write_char);
     /*
@@ -130,7 +132,7 @@ fn main() {
         .expect("Failed to power on bluetooth controller!");
     blue.set_discoverable(true)
         .expect("Failed to make device discoverable!");
-    let adv_idx = match blue.start_advertise(adv) {
+    let adv_idx = match blue.start_adv(adv) {
         Ok(idx) => idx,
         Err((idx, _)) => {
             eprintln!("Warning: failed to start advertisement");
@@ -173,7 +175,7 @@ fn main() {
                     continue;
                 }
             };
-            if out_syncer.borrow().buf != new_clip && *last_written.borrow() != new_clip {
+            if out_syncer.borrow().get_buf() != &new_clip[..] && *last_written.borrow() != new_clip {
                 println!("Writing: {:?}", new_clip);
                 out_syncer.replace(OutSyncer::new(new_clip, verbose));
             }
@@ -196,172 +198,4 @@ fn main() {
     }
 }
 
-struct OutSyncer {
-    buf: Vec<u8>,
-    cur_pos: usize,
-    written: usize,
-    hash: [u8; 32],
-    verbose: u8,
-}
 
-impl OutSyncer {
-    fn new(buf: Vec<u8>, verbose: u8) -> Self {
-        let hash: [u8; 32] = Sha256::digest(&buf).into();
-        OutSyncer {
-            hash,
-            buf,
-            cur_pos: std::usize::MAX,
-            written: 0,
-            verbose,
-        }
-    }
-    fn indicate_local(
-        &mut self,
-        local_char: &mut LocalCharactersitic,
-        max_out: usize,
-    ) -> Result<(), BLEError> {
-        if self.buf.len() == self.cur_pos {
-            return Ok(());
-        }
-        let mut vf = ValOrFn::Value([0; 512], 0);
-        local_char.write_val_or_fn(&mut vf); // remove the vf function so we can write custom data
-        if self.cur_pos == std::usize::MAX {
-            // we need to write the initial data to tell the GATT client how long what we are copying is
-            let mut v = [0; 512];
-            v[..4].copy_from_slice(&std::u32::MAX.to_be_bytes());
-            v[4..8].copy_from_slice(&(self.buf.len() as u32).to_be_bytes());
-            v[8..40].copy_from_slice(&self.hash);
-            local_char.write_val_or_fn(&mut ValOrFn::Value(v, 40));
-            if let Err(e) = local_char.notify() {
-                local_char.write_val_or_fn(&mut vf);
-                return Err(e);
-            }
-            self.written = 0;
-            local_char.write_val_or_fn(&mut vf); // return read function
-            return Ok(());
-        }
-        let target = self.buf.len().min(self.cur_pos + max_out);
-        while self.written < target {
-            let end = target.min(self.written + MAX_APP_MTU - 8);
-            let mut v = [0; 512];
-            let l = end - self.written;
-            v[0..4].copy_from_slice(&self.written.to_be_bytes()[4..8]);
-            v[4..8].copy_from_slice(&l.to_be_bytes()[4..8]);
-            v[8..l + 8].copy_from_slice(&self.buf[self.written..end]);
-            local_char.write_val_or_fn(&mut ValOrFn::Value(v, l));
-            if self.verbose > 0 {
-                eprintln!("Indicating at position {}.", self.written);
-            }
-            if let Err(e) = local_char.notify() {
-                local_char.write_val_or_fn(&mut vf);
-                return Err(e);
-            }
-            self.written = end;
-        }
-        local_char.write_val_or_fn(&mut vf);
-        Ok(())
-    }
-    fn read_fn(&self) -> ([u8; 512], usize) {
-        if self.cur_pos == std::usize::MAX {
-            let mut v = [0; 512];
-            v[..4].copy_from_slice(&std::u32::MAX.to_be_bytes());
-            v[4..8].copy_from_slice(&(self.buf.len() as u32).to_be_bytes());
-            v[8..40].copy_from_slice(&self.hash);
-            (v, 40)
-        } else {
-            let end = self.buf.len().min(self.cur_pos + 256 - 8);
-            let mut v = [0; 512];
-            // let l = end - self.cur_pos;
-            let l = match end.checked_sub(self.cur_pos) {
-                Some(v) => v,
-                None => panic!("overflowed end - self.cur_pos: {} - {}", end, self.cur_pos),
-            };
-            v[0..4].copy_from_slice(&(self.cur_pos as u32).to_be_bytes());
-            v[4..8].copy_from_slice(&(l as u32).to_be_bytes());
-            v[8..l + 8].copy_from_slice(&self.buf[self.cur_pos..end]);
-            (v, l + 8)
-        }
-    }
-    /// If this function returns `true` it indicates there is still data to be written to the client.
-    fn update_pos(&mut self, data: &[u8]) {
-        let mut int_buf = [0; 4];
-        int_buf.copy_from_slice(&data[..4]);
-        let cur_pos = u32::from_be_bytes(int_buf);
-        int_buf.copy_from_slice(&data[4..8]);
-        let msg_length = u32::from_be_bytes(int_buf) as usize;
-        let mut hash = [0; 32];
-        hash.copy_from_slice(&data[8..40]);
-        if msg_length != self.buf.len() || hash != self.hash || cur_pos as usize > self.buf.len() {
-            // handle hash or msg_length mismatch
-            self.cur_pos = std::usize::MAX;
-        } else {
-            // self.dirty = cur_pos as usize != self.buf.len();
-            self.cur_pos = if cur_pos == std::u32::MAX {
-                // we have no received
-                std::usize::MAX
-            } else {
-                let cur_pos = cur_pos as usize;
-                if cur_pos <= self.cur_pos {
-                    // a duplicate ACK was received
-                    self.written = cur_pos
-                }
-                cur_pos
-            };
-        }
-    }
-}
-
-#[derive(Default)]
-struct InSyncer {
-    hash: [u8; 32],
-    msg_length: usize,
-    data_buf: Vec<u8>,
-}
-impl InSyncer {
-    fn read_fn(&self) -> ([u8; 512], usize) {
-        let mut ret = [0; 512];
-        ret[0..4].copy_from_slice(&self.data_buf.len().to_be_bytes()[4..8]);
-        ret[4..8].copy_from_slice(&self.msg_length.to_be_bytes()[4..8]);
-        ret[8..40].copy_from_slice(&self.hash);
-        return (ret, 40);
-    }
-    fn process_write(&mut self, v: &[u8]) -> Result<Option<Vec<u8>>, ()> {
-        let s: &Self = self;
-        if v.len() < 8 {
-            return Err(());
-        }
-        let mut int_buf = [0; 4];
-        int_buf.copy_from_slice(&v[..4]);
-        let p_num = u32::from_be_bytes(int_buf);
-        int_buf.copy_from_slice(&v[4..8]);
-        let p_len = u32::from_be_bytes(int_buf) as usize;
-        if p_num == std::u32::MAX {
-            if v.len() != 40 {
-                let s: &Self = self;
-                return Err(());
-            }
-            self.data_buf.clear();
-            self.msg_length = p_len;
-            self.hash.copy_from_slice(&v[8..])
-        } else {
-            if p_len + 8 != v.len() {
-                return Err(());
-            }
-            if self.data_buf.len() + p_len > self.msg_length {
-                return Err(());
-            }
-            self.data_buf.extend_from_slice(&v[8..]);
-        }
-        if self.data_buf.len() == self.msg_length {
-            let res = Sha256::digest(&self.data_buf);
-            if res.as_slice() == self.hash {
-                Ok(Some(self.data_buf.clone()))
-            } else {
-                self.data_buf.clear();
-                Err(())
-            }
-        } else {
-            Ok(None)
-        }
-    }
-}
